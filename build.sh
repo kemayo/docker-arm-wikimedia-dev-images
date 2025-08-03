ada9363bbb30c7593464f8694d3ff46ec1b329cb
David Lynch <dlynch@wikimedia.org>
2025-08-03 11:35:43 -0500

    Update to produce bookworm images

--- build.sh
+++ build.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 REGISTRY=docker-registry.wikimedia.org
-DISTRO=bullseye
+DISTRO=bookworm
 dateToday=$(date -u +%Y%m%d)
 img="${REGISTRY}/${DISTRO}"
 imgFull="${img}:${dateToday}"
 imgTag="${REGISTRY}/wikimedia-${DISTRO}"
-apache2Img="${REGISTRY}/dev/bullseye-apache2"
-apache2Tag="1.0.0-arm1"
-phpImg="${REGISTRY}/dev/bullseye-php83"
+apache2Img="${REGISTRY}/dev/${DISTRO}-apache2"
+apache2Tag="1.0.1-arm1"
+phpImg="${REGISTRY}/dev/${DISTRO}-php83"
 phpTag="1.0.0"
-phpFpmImg="${REGISTRY}/dev/bullseye-php83-fpm"
-phpFpmTag="2.0.0-arm1"
-phpJobRunnerImg="${REGISTRY}/dev/bullseye-php83-jobrunner"
+phpFpmImg="${REGISTRY}/dev/${DISTRO}-php83-fpm"
+phpFpmTag="1.0.0-arm1"
+phpJobRunnerImg="${REGISTRY}/dev/${DISTRO}-php83-jobrunner"
 phpJobRunnerTag="1.0.0-arm1"
 ## Remove old images
 docker rmi "${imgFull}"
@@ -21,24 +21,24 @@ docker rmi "${apache2Img}:${apache2Tag}"
 docker rmi "${phpImg}:${phpTag}"
 docker rmi "${phpFpmImg}:${phpFpmTag}"
 docker rmi "${phpJobRunnerImg}:${phpJobRunnerTag}"
-## Building Core bullseye image
-echo "Building Core bullseye image ${imgFull}"
-docker build . -f bullseye/Dockerfile -t "${imgFull}"
-## Tagging Core bullseye image
-echo "Tagging Core bullseye image ${img}:latest and ${imgTag}:latest"
+## Building Core image
+echo "Building Core ${DISTRO} image ${imgFull}"
+docker build . -f ${DISTRO}/Dockerfile -t "${imgFull}"
+## Tagging Core image
+echo "Tagging Core ${DISTRO} image ${img}:latest and ${imgTag}:latest"
 docker tag "${imgFull}" "${img}:latest"
 docker tag "${imgFull}" "${imgTag}:latest"
-## Building Apache2 bullseye image
-echo "Building Apache2 bullseye image ${apache2Img}:${apache2Tag}"
+## Building Apache2 image
+echo "Building Apache2 ${DISTRO} image ${apache2Img}:${apache2Tag}"
 docker build . -f apache2/Dockerfile -t "${apache2Img}:${apache2Tag}"
-## Building php8.3 bullseye image
-echo "Building php8.3 bullseye image ${phpImg}:${phpTag}"
+## Building php8.3 image
+echo "Building php8.3 image ${phpImg}:${phpTag}"
 docker build . -f php83/Dockerfile -t "${phpImg}:${phpTag}"
-## Building php8.3 FPM bullseye image
-echo "Building php8.3 FPM bullseye image ${phpFpmImg}:${phpFpmTag}"
+## Building php8.3 FPM image
+echo "Building php8.3 FPM ${DISTRO} image ${phpFpmImg}:${phpFpmTag}"
 docker build . -f fpm/Dockerfile -t "${phpFpmImg}:${phpFpmTag}"
-## Building php8.3 Job Runner bullseye image
-echo "Building php8.3 Job Runner bullseye image ${phpJobRunnerImg}:${phpJobRunnerTag}"
+## Building php8.3 Job Runner image
+echo "Building php8.3 Job Runner ${DISTRO} image ${phpJobRunnerImg}:${phpJobRunnerTag}"
 docker build . -f jobrunner/Dockerfile -t "${phpJobRunnerImg}:${phpJobRunnerTag}"
 
 ## Remove unused images
